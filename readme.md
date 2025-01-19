8327e0feca7e85e3da57b0c2c75c91cc9d05230a
rute-sophie <rute.c.lemos@gmail.com>
2025-01-19 17:24:08 -0500

    Class 2 - added metadata to the mint from class 1

--- readme.md
+++ readme.md
@@ -8,3 +8,7 @@ In this repo I will add the code of the class exercices that I've done.
   - spl_init: [transaction](https://explorer.solana.com/tx/2f4wYubSEk4SDKoLwBgMpT5V5GXFc6fsfv1gJJGK9jenBLWt13UfhJzdwVPNcsmJ8WUwVgS3HHc7rkV2U4oMqNde?cluster=devnet)
   - spl_mint: [create ata](https://explorer.solana.com/tx/3ZNPYmQUUVhrQnwuxr97i4LaSHHtBgjK7drLCYUUqYaxzGzxQSrZ5K6d1dFZcFhQcc526LoXzrtwNFeEKJ74yeyp?cluster=devnet), [mint tokens](https://explorer.solana.com/tx/5oFJFBy74481o795uLvGJ4hMWA7mEM4GrhYyuT2KVYWWrp4JQagF62qjG7xPxKySjwgkG9k3bkn23FFSAWzWX3XR?cluster=devnet)
   - spl_transfer: [create destination ata](https://explorer.solana.com/tx/Z365TaSnxSCRvowma2LZcTV3jB7hQMvvdfGrLcARSdLLZYALrqLZcFCkPVuZd4nquj1tsCxPgHue8NYRFxfDqEw?cluster=devnet), [transfer](https://explorer.solana.com/tx/5sLRakGYSsZ1FVCfdgKRGE25UR8ypEAdTvFBYsYgNNnzxqRGnTVJxnUoCH7nC8zAKm9TatkoP2wS2mY7XEQYEMjb?cluster=devnet)
+
+- Class 2: Added metadata to the mint previously created in class 1
+  - spl_metadata: [transaction](https://explorer.solana.com/tx/RwN4r8hVEVYTxkXC2drycHcxpPpaGwHJwwn5RVoRmXxqVzKEo1HDMctYBQZgpWx4jsxux2HBKMkju33QNG3CLwi?cluster=devnet)
+  
--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,8 +1,8 @@
-import wallet from "../dev-wallet.json"
+import wallet from "./wallet/turbin3-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
+import {
+    createMetadataAccountV3,
+    CreateMetadataAccountV3InstructionAccounts,
     CreateMetadataAccountV3InstructionArgs,
     DataV2Args
 } from "@metaplex-foundation/mpl-token-metadata";
@@ -10,7 +10,7 @@ import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-fo
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("6AN6iUV7Jod61tUUXuHYBxiXqg9oWUy2NbbfpFiGnmVu")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -21,29 +21,38 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint: mint,
+            mintAuthority: signer,
+        };
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "Okami",
+            symbol: "okm",
+            uri: "https://raw.githubusercontent.com/rute-sophie/solana-starter/master/ts/cluster1/resources/mint.json",
+            sellerFeeBasisPoints: 1,
+            creators: null,
+            collection: null,
+            uses: null
+        };
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data,
+            isMutable: false,
+            collectionDetails: null
+        };
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        );
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
-    } catch(e) {
+         let result = await tx.sendAndConfirm(umi);
+         console.log(bs58.encode(result.signature));
+    } catch (e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
 })();
